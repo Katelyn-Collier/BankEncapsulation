9fc1859d76f8c80491fe2a2898f98d89696cf4cb
Katelyn-Collier <katelyncollier14@gmail.com>
2023-01-26 18:20:44 -0600

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance;
+        private double _wbalance;
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Depositing {amount,0:c} into your account...");
+            _balance = amount;
+        }
+        public void Withdraw(double sAmount)
+        {
+            Console.WriteLine($"Withdrawing {sAmount, 0:c}");
+            _wbalance = sAmount;
+        }
+
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        public double NewBalance()
+        {
+            return _balance - _wbalance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine("Welcome to K Bank! What is the amount you would like to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+            double userBalance = account.GetBalance();
+
+            Console.WriteLine($"Current balance: {userBalance,0:c}");
+
+            // below is not part of assignment, just me playing around with what I have learned! would love some tips on how to improve this :)
+
+            Console.WriteLine("Would you like to withdraw an amount? (y/n)");
+
+            string ans;
+            ans = Console.ReadLine();
+
+            if (ans == "y")
+            {
+                Console.WriteLine($"What is the amount you would like to withdraw?");
+                double amountToWithdraw = double.Parse(Console.ReadLine());
+
+                account.Withdraw(amountToWithdraw);
+                double userNewBalance = account.NewBalance();
+
+                Console.WriteLine($"Your new balance is: {userNewBalance,0:c}");
+
+                Console.WriteLine("Thank you for using our services. Goodbye!");
+
+            }
+            else
+            {
+                Console.WriteLine("Thank you for using our services. Goodbye!");
+            }
+
+
+
         }
+
+
+            
     }
 }
